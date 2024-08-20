595c8f5d06d23c07b2cedee8e4829fc6d5c49dd1
Hugo Loiola <hugoximenes07@gmail.com>
2024-08-20 16:44:03 -0300

    Fiori

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,19 +1,51 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
     /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,MessageToast) {
         "use strict";
 
-        return Controller.extend("zhov.controller.View1", {
+        return Controller.extend("zov.controller.View1", {
             onInit: function () {
-
+                //alert("onInit");
             },
 
-            onPress: function () {
-                alert("Hello World!");
-            }   
+            onBeforeRendering: function(){
+                //alert("onBeforeRendering");
+            },
+
+            onAfterRendering: function(){
+                //alert("onAfterRendering");
+            },
+
+            onExit: function(){
+                //alert("onExit");
+            },
+
+            onCalcular: function(){
+                var oView = this.getView();
+                var iB1 = parseInt(oView.byId("b1").getValue());
+                var iB2 = parseInt(oView.byId("b2").getValue());
+                var iB3 = parseInt(oView.byId("b3").getValue());
+                var iB4 = parseInt(oView.byId("b4").getValue());
+                var fResultado = 0;
+                
+                if (iB1, iB2, iB3, iB4) {
+                    fResultado = ( iB1 + iB2 + iB3 + iB4) / 4;
+
+                    oView.byId("resultado").setValue(fResultado);
+
+                    MessageToast.show("Resultado = "+fResultado);
+                } else {
+                    MessageToast.show("Digite números e que sejam maiores que 0");
+                }
+                // MessageToast
+                // sap.m.MessageToast
+                
+                //oView.destroy();
+            }
+        });
     });
-});
