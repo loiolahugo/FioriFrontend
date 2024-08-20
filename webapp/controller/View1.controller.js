92039bb06f4c7b87783c81a485fb2d169ec056ce
Hugo Loiola <hugoximenes07@gmail.com>
2024-08-20 17:21:52 -0300

    Fiori

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,51 +1,59 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,MessageToast) {
+    function (Controller) {
         "use strict";
 
         return Controller.extend("zov.controller.View1", {
             onInit: function () {
-                //alert("onInit");
-            },
-
-            onBeforeRendering: function(){
-                //alert("onBeforeRendering");
-            },
-
-            onAfterRendering: function(){
-                //alert("onAfterRendering");
-            },
-
-            onExit: function(){
-                //alert("onExit");
-            },
-
-            onCalcular: function(){
-                var oView = this.getView();
-                var iB1 = parseInt(oView.byId("b1").getValue());
-                var iB2 = parseInt(oView.byId("b2").getValue());
-                var iB3 = parseInt(oView.byId("b3").getValue());
-                var iB4 = parseInt(oView.byId("b4").getValue());
-                var fResultado = 0;
+                /*
+                // model padrão da view
+                var oView  = this.getView();
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({"usuario": {"nome": "Vinicius"}});
+                oView.setModel(oModel);
+                */
                 
-                if (iB1, iB2, iB3, iB4) {
-                    fResultado = ( iB1 + iB2 + iB3 + iB4) / 4;
+                // model com o nome "dados"
+                var oView  = this.getView();
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({"usuario": {"nome": "José"}});
+                oView.setModel(oModel,"dados");
+            },
 
-                    oView.byId("resultado").setValue(fResultado);
+            onTestModels: function(){
+                // model i18n
+                var oI18n = this.getView().getModel("i18n").getResourceBundle();
+                var sText = oI18n.getText("title");
 
-                    MessageToast.show("Resultado = "+fResultado);
-                } else {
-                    MessageToast.show("Digite números e que sejam maiores que 0");
-                }
-                // MessageToast
-                // sap.m.MessageToast
-                
-                //oView.destroy();
+                console.log("Texto com a chave 'title'");
+                console.log(sText);
+
+                console.log("------------------------------------------");
+
+                // model de usuários
+                var oModel = this.getOwnerComponent().getModel("usuarios");
+                var oData = oModel.getData();
+                console.log("Model dos usuários")
+                console.log(oData);
+
+                console.log("------------------------------------------");
+
+                // model do serviço
+                var oModel = this.getOwnerComponent().getModel();
+                oModel.read("/OVCabSet",{
+                    success: function(oData, oResponse){
+                        console.log("Dados retornados do serviço")
+                        console.log(oData);
+                        console.log(oResponse);
+                    },
+                    error: function(oError){
+                        console.log(oError);
+                    }
+                });
             }
         });
     });
